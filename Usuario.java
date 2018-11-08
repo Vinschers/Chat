89d090ce5dee77d286de8c37a1959c2322a68ed3
Gustavo Miller <42343653+MillerGS03@users.noreply.github.com>
2018-11-08 11:06:50 -0200

    Métodos obrigatórios e nickname do Usuario

--- Usuario.java
+++ Usuario.java
@@ -1,9 +1,70 @@
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Usuario
 {
-    
+    private String nickname;
 
-    public Usuario()
+    private Salas salas;
+    private Socket conexao;
+    private PrintWriter transmissor;
+    private BufferedReader receptor;
+
+    public Usuario(Socket s, Salas salas)
     {
+        this.conexao = s;
+        this.salas = salas;
+    }
+    public String getNickname()
+    {
+        return this.nickname;
+    }
+    public void setNickname(String nickname)
+    {
+        this.nickname = nickname;
+    }
 
+    public String toString() 
+    {
+        return this.nickname;
+    }
+    public boolean equals(Object obj) 
+    {
+        if (obj == null || obj.getClass() != this.getClass())
+            return false;
+        if (obj == this)
+            return true;
+
+        Usuario us = (Usuario) obj;
+
+        if (!this.nickname.equals(us.nickname))
+            return false;
+        if (!this.salas.equals(us.salas))
+            return false;
+        if (!this.conexao.equals(us.conexao))
+            return false;
+        if (!this.transmissor.equals(us.transmissor))
+            return false;
+        if (!this.receptor.equals(us.receptor))
+            return false;
+
+        return true;
+    }
+    public int hashCode() 
+    {
+        int ret = 666;
+
+        ret = 2 * ret + this.nickname.hashCode();
+        ret = 2 * ret + this.salas.hashCode();
+        ret = 2 * ret + this.conexao.hashCode();
+        ret = 2 * ret + this.transmissor.hashCode();
+        ret = 2 * ret + this.receptor.hashCode();
+
+        return ret;
+    }
+    public int compareTo(Usuario outro)
+    {
+        return this.nickname.compareTo(outro.nickname);
     }
 }
