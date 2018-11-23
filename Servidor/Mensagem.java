526b9b7ddf94cd21baff36c65306bbfee4ec06e7
AnnaLBM <42343953+AnnaLBM@users.noreply.github.com>
2018-11-23 11:37:02 -0200

    métodos obrigatórios de Mensagem

--- Servidor/Mensagem.java
+++ Servidor/Mensagem.java
@@ -16,4 +16,30 @@ public class Mensagem extends Enviavel
         return this.mensagem;
     }
     //equals, toString e hashCode
+    public boolean equals(Object o)
+    {
+        if (!super.equals(o))
+            return false;
+        if (o.getClass() != this.getClass())
+            return false;
+        Mensagem m = (Mensagem)o;
+        if (!this.mensagem.equals(m.mensagem))
+            return false;
+        return true;
+    }
+
+    public String toString()
+    {
+        return "[" + super.getHora() + "]" + super.getUsuario() + ": " + this.mensagem;
+    }
+
+    public int hashCode()
+    {
+        int ret = 666;
+
+        ret = 2 * ret + super.hashCode();
+        ret = 5 * ret + this.mensagem.hashCode();
+
+        return ret;
+    }
 }
