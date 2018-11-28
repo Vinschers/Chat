0dc277085e7e6d0ed18e80b07115f2a1ea154122
Scherer <fsvicentin@hotmail.com>
2018-11-28 15:50:31 -0200

    prints retirados da cuidadora

--- Servidor/CuidadoraDeUsuario.java
+++ Servidor/CuidadoraDeUsuario.java
@@ -40,7 +40,6 @@ public class CuidadoraDeUsuario extends Thread
             while(houveErro)
             {
                 houveErro = false;
-                System.out.println("Esperando o nome");
                 nomeEscolhido = (String)ois.readObject();
                 nomeSala = (String)ois.readObject();
                 salaEscolhida = null;
@@ -86,22 +85,12 @@ public class CuidadoraDeUsuario extends Thread
             oos.writeObject("ok");
             oos.flush();
             this.usuario = new Usuario(this.conexao, oos, ois, nomeEscolhido, salaEscolhida);
-
-            System.out.println("Usuario " + nomeEscolhido + " criado. Iniciando o loop com " + us.size() + " usuario(s) ja conectados.");
-            
             salaEscolhida.adicionarUsuario(this.usuario);
             for (int i = 0; i < us.size() - 1; i++)
             {
-                System.out.println(nomeEscolhido + " mandando para " + us.get(i).getNickname());
                 this.usuario.envia(new AvisoDeEntradaNaSala(), us.get(i).getNickname());
-                System.out.println("Mandou. Agora " + us.get(i).getNickname() + " esta mandando para " + nomeEscolhido);
                 us.get(i).envia(new AvisoDeEntradaNaSala(), this.usuario.getNickname());
-                System.out.println("Mandou");
             }
-            // Fazer várias vezes this.usuario.envia(new AvisoDeEntradaNaSala(i)), onde i é o nome de algum usuário na sala
-            // Fazer várias vezes i.envia(new AvisoDeEntradaNaSala(usuario.getNome())), onde i é o nome de algum usuário na sala
-            // Incluir o usuario na sala
-
             Enviavel recebido = null;
             Mensagem aux;
             ArrayList<String> dest;
@@ -116,13 +105,8 @@ public class CuidadoraDeUsuario extends Thread
                     for (int i = 0; i < dest.size(); i++)
                         this.usuario.envia(aux, dest.get(i));
                 }
-                // receber mensagens, avisos de entrada na e de saida da sala
-                // se for mensagem, pega nela o destinatario, acha o destinatario na sala e manda para ele a mensagem
             }
             while (!(recebido instanceof PedidoParaSairDaSala));
-
-            // Remover this.usuario da sala
-            // Mandar para todos da sala: new AvisoDeSaidaDaSala(this.usuario.getNome())
             salaEscolhida.removerUsuario(this.usuario);
             us = salaEscolhida.getUsuarios();
             for (int i = 0; i < us.size(); i++)
