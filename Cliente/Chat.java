7744920c8f4a4db85cbe5f014a25cdcb30474866
Scherer <fsvicentin@hotmail.com>
2018-12-01 16:58:06 -0200

    nnnnnnnnnnnnnnnnnnnnnn
    wrap adicionado

--- Cliente/Chat.java
+++ Cliente/Chat.java
@@ -1,4 +1,12 @@
 import java.awt.*;
+import java.awt.Dimension; 
+import javax.swing.*; 
+import javax.swing.text.Element; 
+import javax.swing.text.View; 
+import javax.swing.text.ViewFactory; 
+import javax.swing.text.html.HTMLEditorKit; 
+import javax.swing.text.html.InlineView; 
+import javax.swing.text.html.ParagraphView;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Rectangle;
@@ -219,7 +227,54 @@ public class Chat extends JFrame {
 		panel_2.add(label_2, BorderLayout.EAST);
 		
 		this.folhaDeEstilo = new StyleSheet();
-		this.editor = new HTMLEditorKit();
+		this.editor = new HTMLEditorKit(){ 
+			@Override 
+			public ViewFactory getViewFactory(){ 
+  
+				return new HTMLFactory(){ 
+					public View create(Element e){ 
+					   	View v = super.create(e); 
+					   	if(v instanceof InlineView){ 
+						   	return new InlineView(e){ 
+							   	public int getBreakWeight(int axis, float pos, float len) { 
+									return GoodBreakWeight; 
+							   	} 
+							   	public View breakView(int axis, int p0, float pos, float len) { 
+								   	if(axis == View.X_AXIS) { 
+									   	checkPainter(); 
+									   	int p1 = getGlyphPainter().getBoundedPosition(this, p0, pos, len); 
+									   	if(p0 == getStartOffset() && p1 == getEndOffset()) { 
+											return this; 
+									   	} 
+									   	return createFragment(p0, p1); 
+								   	} 
+								   	return this; 
+								} 
+							}; 
+					   	} 
+					   	else if (v instanceof ParagraphView) { 
+						   	return new ParagraphView(e) { 
+							   	protected SizeRequirements calculateMinorAxisRequirements(int axis, SizeRequirements r) { 
+								   	if (r == null) { 
+										r = new SizeRequirements(); 
+								   	} 
+								   	float pref = layoutPool.getPreferredSpan(axis); 
+								   	float min = layoutPool.getMinimumSpan(axis); 
+								   	// Don't include insets, Box.getXXXSpan will include them. 
+									r.minimum = (int)min; 
+									r.preferred = Math.max(r.minimum, (int) pref); 
+									r.maximum = Integer.MAX_VALUE; 
+									r.alignment = 0.5f; 
+								   	return r; 
+								} 
+  
+							}; 
+						} 
+					   	return v; 
+					} 
+				}; 
+			} 
+		};
 
 		this.folhaDeEstilo.addRule("body {background-color: #004b66; font-size: 16pt; font-family: Century Gothic; color: white; max-width: 500px; overflow-x: hidden;}");
 		this.folhaDeEstilo.addRule("p {padding: 2px;} ");
@@ -355,3 +410,95 @@ public class Chat extends JFrame {
 		ultimaMensagem = (recebido instanceof Mensagem?(Mensagem)recebido:null);
 	}
 }
+//teste de wrap
+/*
+import java.awt.Dimension; 
+import javax.swing.*; 
+import javax.swing.text.Element; 
+import javax.swing.text.View; 
+import javax.swing.text.ViewFactory; 
+import javax.swing.text.html.HTMLEditorKit; 
+import javax.swing.text.html.InlineView; 
+import javax.swing.text.html.ParagraphView; 
+ 
+public class HtmlLetterWrap { 
+ 
+    public HtmlLetterWrap(){ 
+        final JFrame frame = new JFrame("Letter wrap test"); 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+ 
+        final JEditorPane htmlTextPane = newJEditorPane(); 
+ 
+        htmlTextPane.setEditorKit(new HTMLEditorKit(){ 
+           @Override 
+           public ViewFactory getViewFactory(){ 
+ 
+               return new HTMLFactory(){ 
+                   public View create(Element e){ 
+                      View v = super.create(e); 
+                      if(v instanceof InlineView){ 
+                          return new InlineView(e){ 
+                              public int getBreakWeight(int axis, float pos, float len) { 
+                                  return GoodBreakWeight; 
+                              } 
+                              public View breakView(int axis, int p0, float pos, float len) { 
+                                  if(axis == View.X_AXIS) { 
+                                      checkPainter(); 
+                                      int p1 = getGlyphPainter().getBoundedPosition(this, p0, pos, len); 
+                                      if(p0 == getStartOffset() && p1 == getEndOffset()) { 
+                                          return this; 
+                                      } 
+                                      return createFragment(p0, p1); 
+                                  } 
+                                  return this; 
+                                } 
+                            }; 
+                      } 
+                      else if (v instanceof ParagraphView) { 
+                          return new ParagraphView(e) { 
+                              protected SizeRequirements calculateMinorAxisRequirements(int axis, SizeRequirements r) { 
+                                  if (r == null) { 
+                                        r = new SizeRequirements(); 
+                                  } 
+                                  float pref = layoutPool.getPreferredSpan(axis); 
+                                  float min = layoutPool.getMinimumSpan(axis); 
+                                  // Don't include insets, Box.getXXXSpan will include them. 
+                                    r.minimum = (int)min; 
+                                    r.preferred = Math.max(r.minimum, (int) pref); 
+                                    r.maximum = Integer.MAX_VALUE; 
+                                    r.alignment = 0.5f; 
+                                  return r; 
+                                } 
+ 
+                            }; 
+                        } 
+                      return v; 
+                    } 
+                }; 
+            } 
+        }); 
+ 
+        htmlTextPane.setContentType("text/html"); 
+        htmlTextPane.setText("This text pane contains html. The custom HTMLEditorKit supports single letter wrapping."); 
+ 
+        JEditorPane noHtmlTextPane = new JEditorPane(); 
+        noHtmlTextPane.setText("This text pane contains no html. It supports single letter wrapping!"); 
+ 
+        htmlTextPane.setMinimumSize(new Dimension(0, 0)); 
+        noHtmlTextPane.setMinimumSize(new Dimension(0, 0)); 
+ 
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, noHtmlTextPane, htmlTextPane); 
+        splitPane.setContinuousLayout(true); 
+ 
+        frame.add(splitPane); 
+ 
+        frame.setSize(200, 200); 
+        frame.setVisible(true); 
+        splitPane.setDividerLocation(.5); 
+    } 
+ 
+  public static void main(String[] args) { 
+      newHtmlLetterWrap(); 
+  } 
+}
+	*/
